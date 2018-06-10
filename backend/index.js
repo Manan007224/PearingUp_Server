79821bc509532691b6fcfde515d05e00cffd67c2
Manan007224 <maniyarmanan1996@gmail.com>
2018-06-10 00:21:44 -0700

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -72,7 +72,7 @@ function isLoggedIn(req, res, next) {
 require('./config/passport')(passport);
 
 const hostname = 'localhost';
-const port = process.env.port || 8000;
+const port = process.env.PORT || 5000;
 
 const server = app.listen(port, hostname, () => {
 	mongoose.connect("mongodb://127.0.0.1/login_schema", dbOptions, (err) => {
