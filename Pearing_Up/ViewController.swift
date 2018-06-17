c14213501be1f6cb6da0a1c56d1b8506b44f8de4
Manan Maniyar <maniyarmanan1996@gmail.com>
2018-06-17 02:58:12 -0700

    trying alamofire syntax

--- Pearing_Up/ViewController.swift
+++ Pearing_Up/ViewController.swift
@@ -2,24 +2,44 @@
 //  ViewController.swift
 //  Pearing_Up
 //
-//  Created by Navin Kumar Ravindra on 2018-06-16.
+//  Created by Manan Maniyar 2018-06-16.
 //  Copyright © 2018 Manan Maniyar. All rights reserved.
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
+    
+    let server_URL = "https://pearingup.herokuapp.com/"
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getLogin(url: server_URL)
     }
+    
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    func getLogin(url: String){
+        Alamofire.request(url, method: .get).responseJSON {
+            response in
+            if response.result.isSuccess {
+                print("Response Successfull")
+                print("Response Response")
+            }
+            else {
+                print("Error in the request")
+            }
+        }
+    }
 }
 
