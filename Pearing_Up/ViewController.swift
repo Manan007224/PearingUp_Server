4cb11924c4d2c4892bf12967e360073367161a40
Manan Maniyar <maniyarmanan1996@gmail.com>
2018-06-16 23:34:38 -0700

    Initial Commit

---
+++ Pearing_Up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pearing_Up
+//
+//  Created by Navin Kumar Ravindra on 2018-06-16.
+//  Copyright © 2018 Manan Maniyar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
