6287448ef10981c0181eb545363d0c5135022e0c
Manan Maniyar <maniyarmanan1996@gmail.com>
2018-06-18 03:57:19 -0700

    added basic login controller server calls

--- Pearing_Up/LoginViewController.swift
+++ Pearing_Up/LoginViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var login_tet: UILabel!
+    @IBOutlet weak var pwd_holder: UITextField!
+    @IBOutlet weak var email_holder: UITextField!
+    let login_url = "https://pearingup.herokuapp.com/login"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +28,25 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func login_click(_ sender: Any) {
+        let user_params: [String:String] = ["email": email_holder.text!, "password": pwd_holder.text!]
+        login(url: login_url, params: user_params)
+    }
+    
+    func login(url: String, params: [String:String]){
+        Alamofire.request(url, method: .post, parameters: params).responseJSON{
+            response in
+            if response.result.isSuccess {
+                let temp : JSON = JSON(response.result.value!)
+                print(temp)
+                self.performSegue(withIdentifier: "ProfileSegue", sender: self)
+            }
+            else {
+                print("Error Happened")
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
